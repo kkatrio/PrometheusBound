ec0d6cf021d63c64e3becc13255cce1886687a7c
konstantinos katrioplas <kokatrio@gmail.com>
2020-11-22 21:06:09 +0200

    serial server acts on status

--- serial_server.go
+++ serial_server.go
@@ -1,16 +1,22 @@
 package main
 
 import (
-        "fmt"
-		"log"
-		"net/http"
-		"html"
-        "github.com/tarm/serial"
+	"fmt"
+	"log"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"github.com/tarm/serial"
+	"strings"
 )
 
 func send_serial(event string) {
-	c := &serial.Config{Name: "/dev/ttyUSB1", Baud: 9600}
+	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 9600}
 	s, err := serial.OpenPort(c)
+	if err != nil {
+		c := &serial.Config{Name: "/dev/ttyUSB1", Baud: 9600}
+		s, err = serial.OpenPort(c)
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -20,16 +26,43 @@ func send_serial(event string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("n: ", n)
+	_ = n // avoid declared but not used error
 	s.Close()
+}
+
+type Message struct {
+	Status string
 }
 
 func main() {
 
 	http.HandleFunc("/alarm", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Sending: %q", html.EscapeString(r.URL.Path))
-		send_serial("on")
+
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		//fmt.Println("body:", string(body))
+
+		var m Message
+		dec := json.NewDecoder(strings.NewReader(string(body))) // should be able to use r.body directly
+		err = dec.Decode(&m)
+		if err != nil {
+			log.Fatalf("could not decode", err)
+		}
+		log.Printf("alert status: %s\nsending serial\n", m.Status)
+
+		if m.Status == "firing" {
+			send_serial("1")
+			fmt.Println("fired")
+		} else if m.Status == "resolved" {
+			send_serial("0")
+			fmt.Println("resolved")
+		} else {
+			log.Println("status ?")
+		}
 	})
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
